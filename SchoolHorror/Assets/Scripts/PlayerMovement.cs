4ac44844e43a17a425779d871cd565f06b7d2b63
EliTE08 <memeinsta84@gmail.com>
2024-05-26 19:20:55 -0400

    Added Player and Movement Script
    Doesn't move yet...

---
+++ SchoolHorror/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,12 @@
+using System.Numerics;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    void Start(){
+    }
+
+    void Update(){
+    }
+}
